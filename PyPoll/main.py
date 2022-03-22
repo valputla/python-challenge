a0c02fd024a88f7eb2feb439b7c6ca3792456f85
Lindsey Morales <lindseyvmorales@gmail.com>
2022-03-21 21:17:08 -0500

    Found total votes for Charles Casper Stockham

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,14 +16,29 @@ with open(csvpath) as csvfile:
 
     total_votes = 0
     candidates = []
+    charlie = []
+    total_votes_CCS = 0
+    total_votes_DD = 0
+    total_votes_RAD = 0
     for row in csvreader:
         total_votes += 1
         if row[2] not in candidates:
             candidates.append(row[2])
+        if row[2] == "Charles Casper Stockham":
+            total_votes_CCS += 1
+print(total_votes_CCS)
 
-print(candidates)
+
 
 
 
 
 #print(total_votes)
+#print(candidates)
+
+
+# print("Election Results")
+# print("-----------------------------")
+# print(f"Total Votes: {total_votes}")
+# print("-----------------------------")
+# print(f"Charles Casper Stockham: ")
