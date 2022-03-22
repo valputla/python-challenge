6f62291f751aeaf00e245cd3f0d56c040e78211e
Lindsey Morales <lindseyvmorales@gmail.com>
2022-03-21 21:47:07 -0500

    Found the candidate who won by popular vote.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,25 +16,39 @@ with open(csvpath) as csvfile:
 
     total_votes = 0
     candidates = []
-    charlie = []
     total_votes_CCS = 0
     total_votes_DD = 0
     total_votes_RAD = 0
+    
     for row in csvreader:
         total_votes += 1
         if row[2] not in candidates:
             candidates.append(row[2])
+            
         if row[2] == "Charles Casper Stockham":
             total_votes_CCS += 1
+            
         if row[2] == "Diana DeGette":
             total_votes_DD += 1
+            
         if row[2] == "Raymon Anthony Doane":
             total_votes_RAD += 1
+        
     
     percent_CCS = round(total_votes_CCS/total_votes * 100, 3)
     percent_DD = round(total_votes_DD/total_votes * 100, 3)
     percent_RAD = round(total_votes_RAD/total_votes * 100, 3)
 
+
+candidate_votes = [total_votes_CCS, total_votes_DD, total_votes_RAD]
+#print(max(candidate_votes))
+
+if max(candidate_votes) == 85213:
+    print("Charles Casper Stockham")
+elif max(candidate_votes) == 272892:
+    print("Diana DeGette")
+else:
+    print("Raymon Anthony Doane")
 
 
 
@@ -46,9 +60,9 @@ with open(csvpath) as csvfile:
 # print(total_votes_DD)
 # print(total_votes_RAD)
 # print(percent_CCS)
-print(percent_CCS)
-print(percent_DD)
-print(percent_RAD)
+# print(percent_CCS)
+# print(percent_DD)
+# print(percent_RAD)
 
 
 # print("Election Results")
